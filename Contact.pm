fbccf0b337524d0354bb8e68d5fd1922d99a4a9d
SebDeclercq <miniSeb3.0@gmail.com>
2011-12-19 17:38:47 +0100

    w/ perltidy

--- Contact.pm
+++ Contact.pm
@@ -6,32 +6,32 @@ __PACKAGE__->load_components(qw/ Core /);
 __PACKAGE__->table('contacts');
 
 __PACKAGE__->add_columns(
-'id' => {
-  'data_type' => 'bigint',
-  'is_auto_increment' => 1,
-  'default_value' => undef,
-  'is_foreign_key' => 0,
-  'name' => 'id',
-  'is_nullable' => 0
-},
-'phone' => {
-  'data_type' => 'text',
-  'is_auto_increment' => 0,
-  'default_value' => undef,
-  'is_foreign_key' => 0,
-  'name' => 'name',
-  'is_nullable' => 0
-},
-'mail' => {
-  'data_type' => 'text',
-  'is_auto_increment' => 0,
-  'default_value' => undef,
-  'is_foreign_key' => 1,
-  'name' => 'contacts',
-  'is_nullable' => 1
-}
+    'id' => {
+        'data_type'         => 'bigint',
+        'is_auto_increment' => 1,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'id',
+        'is_nullable'       => 0
+    },
+    'phone' => {
+        'data_type'         => 'text',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'name',
+        'is_nullable'       => 0
+    },
+    'mail' => {
+        'data_type'         => 'text',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 1,
+        'name'              => 'contacts',
+        'is_nullable'       => 1
+    }
 );
 
-__PACKAGE__->set_primary_key( 'id' );
+__PACKAGE__->set_primary_key('id');
 
 1;
--- Pers.pm
+++ Pers.pm
@@ -6,32 +6,32 @@ __PACKAGE__->load_components(qw/ Core /);
 __PACKAGE__->table('pers');
 
 __PACKAGE__->add_columns(
-'id' => {
-  'data_type' => 'bigint',
-  'is_auto_increment' => 1,
-  'default_value' => undef,
-  'is_foreign_key' => 0,
-  'name' => 'id',
-  'is_nullable' => 0
-},
-'name' => {
-  'data_type' => 'text',
-  'is_auto_increment' => 0,
-  'default_value' => undef,
-  'is_foreign_key' => 0,
-  'name' => 'name',
-  'is_nullable' => 0
-},
-'contacts' => {
-  'data_type' => 'bigint',
-  'is_auto_increment' => 0,
-  'default_value' => undef,
-  'is_foreign_key' => 1,
-  'name' => 'contacts',
-  'is_nullable' => 0
-}
+    'id' => {
+        'data_type'         => 'bigint',
+        'is_auto_increment' => 1,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'id',
+        'is_nullable'       => 0
+    },
+    'name' => {
+        'data_type'         => 'text',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 0,
+        'name'              => 'name',
+        'is_nullable'       => 0
+    },
+    'contacts' => {
+        'data_type'         => 'bigint',
+        'is_auto_increment' => 0,
+        'default_value'     => undef,
+        'is_foreign_key'    => 1,
+        'name'              => 'contacts',
+        'is_nullable'       => 0
+    }
 );
 
-__PACKAGE__->set_primary_key( 'id' );
+__PACKAGE__->set_primary_key('id');
 
 1;
